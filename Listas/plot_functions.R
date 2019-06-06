c541ef0b00e63b6e83706154f8d8c21c26ffa976
Victor D P Cunha <victordallapria@hotmail.com>
2019-06-06 13:54:23 -0300

    meh

--- Listas/plot_functions.R
+++ Listas/plot_functions.R
@@ -1,17 +1,17 @@
 plot_prediction <- function(model, response, response_name = "resposta") {
-  # precisa de dplyr, broom, ggplot2
+  # precisa de dplyr, ggplot2
   
   p <- predict.lm(model, interval = "prediction") %>% 
-    dplyr::as_tibble() %>% mutate(response = response) %>% 
-    ggplot(aes(fit, response)) + 
-    geom_point(col = "cadetblue") + 
-    geom_line(aes(y = fit), col = "chocolate") + 
-    geom_ribbon(aes(ymin = lwr, ymax = upr), fill = "coral", alpha = 0.3) + 
-    labs(x = "predição", y = response_name) + theme_classic()
+    dplyr::as_tibble() %>% dplyr::mutate(response = response) %>% 
+    ggplot2::ggplot() + 
+    ggplot2::geom_line(aes(fit, fit), col = "chocolate") + 
+    ggplot2::geom_point(aes(fit, response), col = "cadetblue") + 
+    ggplot2::geom_ribbon(aes(x = fit, ymin = lwr, ymax = upr), fill = "coral", alpha = 0.3) + 
+    ggplot2::labs(x = "predição", y = response_name) + ggplot2::theme_classic()
   p
 }
 
-plot_residuals <- function(model) {
+plot_residuals <- function(model, binwidth = NULL, bins = NULL) {
   # precisa de ggplot2, gridExtra, qqplotr
   
   residuals <- data.frame(
@@ -19,14 +19,21 @@ plot_residuals <- function(model) {
     fitted = model$fitted.values, 
     index = 1:length(model$fitted.values)
   )
-  p <- ggplot(residuals) + labs(y = "resíduo studentizado") + theme_classic()
+  
+  p <- ggplot2::ggplot(residuals) + 
+    ggplot2::labs(y = "resíduo studentizado") + ggplot2::theme_classic()
+  
   gridExtra::grid.arrange(
-    p + geom_point(aes(x = index, y = residual), col = "gray30", alpha = 0.80) + labs(x = "índice"), 
-    p + geom_point(aes(x = fitted, y = residual), col = "gray30", alpha = 0.80) + labs(x = "índice"), 
-    p + geom_histogram(aes(x = residual), fill = "gray30", col = "gray80"), 
-    ggplot(residuals, aes(sample = residual)) + 
-      qqplotr::stat_qq_band(bandType = "pointwise") + qqplotr::stat_qq_line() + qqplotr::stat_qq_point() + 
-      labs(x = "quantil teórico", y = "quantil amostral") + theme_classic(), 
+    p + ggplot2::geom_point(aes(x = index, y = residual), col = "gray30", alpha = 0.80) + 
+      ggplot2::labs(x = "índice"), 
+    p + ggplot2::geom_point(aes(x = fitted, y = residual), col = "gray30", alpha = 0.80) + 
+      ggplot2::labs(x = "índice"), 
+    p + ggplot2::geom_histogram(aes(x = residual), fill = "gray30", col = "gray80"), 
+    ggplot2::ggplot(residuals, aes(sample = residual)) + 
+      qqplotr::stat_qq_band(bandType = "pointwise") + 
+      qqplotr::stat_qq_line() + 
+      qqplotr::stat_qq_point() + 
+      ggplot2::labs(x = "quantil teórico", y = "quantil amostral") + ggplot2::theme_classic(), 
     nrow = 2
   )
 }
