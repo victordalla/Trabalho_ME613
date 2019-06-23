99d3dd4ca0c0e684d809531da686b934fb0a9b70
Victor D P Cunha <victordallapria@hotmail.com>
2019-06-23 17:53:24 -0300

    leticia havia removido todos os acentos...

--- Listas/plot_functions.R
+++ Listas/plot_functions.R
@@ -7,7 +7,7 @@ plot_prediction <- function(model, response, response_name = "resposta") {
     ggplot2::geom_line(aes(fit, fit), col = "chocolate") + 
     ggplot2::geom_point(aes(fit, response), col = "cadetblue") + 
     ggplot2::geom_ribbon(aes(x = fit, ymin = lwr, ymax = upr), fill = "coral", alpha = 0.3) + 
-    ggplot2::labs(x = "predicao", y = response_name) + ggplot2::theme_bw()
+    ggplot2::labs(x = "predição", y = response_name) + ggplot2::theme_bw()
   p
 }
 
@@ -21,11 +21,11 @@ plot_residuals <- function(model, binwidth = NULL, bins = NULL) {
   )
   
   p <- ggplot2::ggplot(residuals) + 
-    ggplot2::labs(y = "residuo studentizado") + ggplot2::theme_bw()
+    ggplot2::labs(y = "resíduo studentizado") + ggplot2::theme_bw()
   
   gridExtra::grid.arrange(
     p + ggplot2::geom_point(aes(x = index, y = residual), col = "gray30", alpha = 0.80) + 
-      ggplot2::labs(x = "indice"), 
+      ggplot2::labs(x = "índice"), 
     p + ggplot2::geom_point(aes(x = fitted, y = residual), col = "gray30", alpha = 0.80) + 
       ggplot2::labs(x = "valores ajustados"), 
     p + ggplot2::geom_histogram(aes(x = residual), fill = "gray30", col = "gray80"), 
@@ -33,7 +33,7 @@ plot_residuals <- function(model, binwidth = NULL, bins = NULL) {
       qqplotr::stat_qq_band(bandType = "pointwise") + 
       qqplotr::stat_qq_line() + 
       qqplotr::stat_qq_point(col = "gray20", alpha = 0.80) + 
-      ggplot2::labs(x = "quantil teorico", y = "quantil amostral") + ggplot2::theme_bw(), 
+      ggplot2::labs(x = "quantil teórico", y = "quantil amostral") + ggplot2::theme_bw(), 
     nrow = 2
   )
 }
