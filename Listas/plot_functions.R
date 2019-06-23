8900e3763295b93cc7e88984a048969012303c68
Victor D P Cunha <victordallapria@hotmail.com>
2019-06-23 16:34:08 -0300

    theme_classic -> bw, como combinado

--- Listas/plot_functions.R
+++ Listas/plot_functions.R
@@ -7,7 +7,7 @@ plot_prediction <- function(model, response, response_name = "resposta") {
     ggplot2::geom_line(aes(fit, fit), col = "chocolate") + 
     ggplot2::geom_point(aes(fit, response), col = "cadetblue") + 
     ggplot2::geom_ribbon(aes(x = fit, ymin = lwr, ymax = upr), fill = "coral", alpha = 0.3) + 
-    ggplot2::labs(x = "predicao", y = response_name) + ggplot2::theme_classic()
+    ggplot2::labs(x = "predicao", y = response_name) + ggplot2::theme_bw()
   p
 }
 
@@ -21,7 +21,7 @@ plot_residuals <- function(model, binwidth = NULL, bins = NULL) {
   )
   
   p <- ggplot2::ggplot(residuals) + 
-    ggplot2::labs(y = "residuo studentizado") + ggplot2::theme_classic()
+    ggplot2::labs(y = "residuo studentizado") + ggplot2::theme_bw()
   
   gridExtra::grid.arrange(
     p + ggplot2::geom_point(aes(x = index, y = residual), col = "gray30", alpha = 0.80) + 
@@ -33,7 +33,7 @@ plot_residuals <- function(model, binwidth = NULL, bins = NULL) {
       qqplotr::stat_qq_band(bandType = "pointwise") + 
       qqplotr::stat_qq_line() + 
       qqplotr::stat_qq_point(col = "gray20", alpha = 0.80) + 
-      ggplot2::labs(x = "quantil teorico", y = "quantil amostral") + ggplot2::theme_classic(), 
+      ggplot2::labs(x = "quantil teorico", y = "quantil amostral") + ggplot2::theme_bw(), 
     nrow = 2
   )
 }
